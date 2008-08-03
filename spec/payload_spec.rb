bc0e303e74938ef78b414b18c5b080cb04c4e04d
Adam Wiggins <adam@heroku.com>
2008-08-03 16:09:19 -0700

    convert payload spec to rspec

--- spec/payload_spec.rb
+++ spec/payload_spec.rb
@@ -1,46 +1,42 @@
 require File.dirname(__FILE__) + "/base"
 
-context "A regular Payload" do
+describe RestClient::Payload do
+	context "A regular Payload" do
+		it "should should default content-type to standard enctype" do
+			RestClient::Payload::UrlEncoded.new({}).headers['Content-Type'].
+				should == 'application/x-www-form-urlencoded'
+		end
 
-	specify "should should default content-type to standard enctype" do
-	  RestClient::Payload::UrlEncoded.new({}).headers['Content-Type'].
-			should == 'application/x-www-form-urlencoded'
+		it "should form properly encoded params" do
+			RestClient::Payload::UrlEncoded.new({:foo => 'bar'}).to_s.
+				should == "foo=bar"
+		end
 	end
 
-	specify "should form properly encoded params" do
-		 RestClient::Payload::UrlEncoded.new({:foo => 'bar'}).to_s.
-			should == "foo=bar"
-	end
+	context "A multipart Payload" do
+		it "should should default content-type to standard enctype" do
+			m = RestClient::Payload::Multipart.new({})
+			m.stub!(:boundary).and_return(123)
+			m.headers['Content-Type'].should == 'multipart/form-data; boundary="123"'
+		end
 
-end
-
-context "A multipart Payload" do
-	specify "should should default content-type to standard enctype" do
-		m = RestClient::Payload::Multipart.new({})
-		m.stub!(:boundary).and_return(123)
-		m.headers['Content-Type'].should == 'multipart/form-data; boundary="123"'
-	end
-
-	xspecify "should form properly seperated multipart data" do
-		m = RestClient::Payload::Multipart.new({:foo => "bar"})
-		m.stub!(:boundary).and_return("123")
-		m.to_s.should == <<-EOS
+		xit "should form properly seperated multipart data" do
+			m = RestClient::Payload::Multipart.new({:foo => "bar"})
+			m.stub!(:boundary).and_return("123")
+			m.to_s.should == <<-EOS
 --123\r
 Content-Disposition: multipart/form-data; name="foo"\r
 \r
 bar\r
 --123--\r
 EOS
-	end
+		end
 
-	xspecify "should form properly seperated multipart data" do
-		f = File.new(File.dirname(__FILE__) + "/master_shake.jpg")
-		# f = mock("master_shake.jpg")
-		# f.stub!(:path).and_return("master_shake.jpg")
-		# f.stub!(:read).and_return("datadatadata")
-		m = RestClient::Payload::Multipart.new({:foo => f})
-		m.stub!(:boundary).and_return("123")
-		m.to_s.should == <<-EOS
+		xit "should form properly seperated multipart data" do
+			f = File.new(File.dirname(__FILE__) + "/master_shake.jpg")
+			m = RestClient::Payload::Multipart.new({:foo => f})
+			m.stub!(:boundary).and_return("123")
+			m.to_s.should == <<-EOS
 --123\r
 Content-Disposition: multipart/form-data; name="foo"; filename="master_shake.jpg"\r
 Content-Type: image/jpeg\r
@@ -48,31 +44,26 @@ Content-Type: image/jpeg\r
 datadatadata\r
 --123--\r
 EOS
+		end
 	end
 
-end
+	context "Payload generation" do
+		it "should recognize standard urlencoded params" do
+			RestClient::Payload.generate({"foo" => 'bar'}).should be_kind_of(RestClient::Payload::UrlEncoded)
+		end
+		
+		it "should recognize multipart params" do
+			f = File.new(File.dirname(__FILE__) + "/master_shake.jpg")
 
-context "Payload generation" do
-	
-	specify "should recognize standard urlencoded params" do
-		RestClient::Payload.generate({"foo" => 'bar'}).should be_kind_of(RestClient::Payload::UrlEncoded)
-	end
-	
-	specify "should recognize multipart params" do
-		# f = mock("master_shake.jpg")
-		# f.stub!(:path)
-		# f.stub!(:read)
-		f = File.new(File.dirname(__FILE__) + "/master_shake.jpg")
+			RestClient::Payload.generate({"foo" => f}).should be_kind_of(RestClient::Payload::Multipart)
+		end
 		
-		RestClient::Payload.generate({"foo" => f}).should be_kind_of(RestClient::Payload::Multipart)
+		it "should be multipart if forced" do
+			RestClient::Payload.generate({"foo" => "bar", :multipart => true}).should be_kind_of(RestClient::Payload::Multipart)
+		end
+
+		it "should return data if no of the above" do
+		  RestClient::Payload.generate("data").should be_kind_of(RestClient::Payload::Base)
+		end
 	end
-	
-	specify "should be multipart if forced" do
-		RestClient::Payload.generate({"foo" => "bar", :multipart => true}).should be_kind_of(RestClient::Payload::Multipart)
-	end
-	
-	specify "should return data if no of the above" do
-	  RestClient::Payload.generate("data").should be_kind_of(RestClient::Payload::Base)
-	end
-		
 end
