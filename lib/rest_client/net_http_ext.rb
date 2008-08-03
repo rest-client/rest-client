640724f6d4e295b9c54d8465b7e75e3e989e7844
Adam Wiggins <adam@heroku.com>
2008-08-03 15:46:24 -0700

    i'm fussy about whitespace

--- lib/rest_client/net_http_ext.rb
+++ lib/rest_client/net_http_ext.rb
@@ -2,20 +2,20 @@
 # Replace the request method in Net::HTTP to sniff the body type
 # and set the stream if appropriate
 #
-# Taken from:  
+# Taken from:	
 # http://www.missiondata.com/blog/ruby/29/streaming-data-to-s3-with-ruby/
 
 module Net
-  class HTTP
-    alias __request__ request
+	class HTTP
+		alias __request__ request
 
-    def request(req, body = nil, &block)
-      if body != nil && body.respond_to?(:read)
-        req.body_stream = body
-        return __request__(req, nil, &block)
-      else
-        return __request__(req, body, &block)
-      end
-    end
-  end
+		def request(req, body=nil, &block)
+			if body != nil && body.respond_to?(:read)
+				req.body_stream = body
+				return __request__(req, nil, &block)
+			else
+				return __request__(req, body, &block)
+			end
+		end
+	end
 end
--- lib/rest_client/payload.rb
+++ lib/rest_client/payload.rb
@@ -2,11 +2,8 @@ require "tempfile"
 require "stringio"
 
 module RestClient
-
 	module Payload
 		extend self
-
-		class NotImplemented < RuntimeError; end
 
 		def generate(params)
 			if params.is_a?(String)
@@ -20,62 +17,57 @@ module RestClient
 		end
 
 		class Base
-
 			def initialize(params)
 				build_stream(params)
 			end
-			
+
 			def build_stream(params)
 				@stream = StringIO.new(params)
 				@stream.seek(0)
 			end
-			
+
 			def read(bytes=nil)
 				@stream.read(bytes)
 			end
 			alias :to_s :read
-						
+
 			def escape(v)
 				URI.escape(v.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
 			end
 
 			def headers
-				{'Content-Length' => size.to_s}
+				{ 'Content-Length' => size.to_s }
 			end
-			
+
 			def size
 				@stream.size
 			end
 			alias :length :size
-			
+
 			def close
 				@stream.close
 			end
-
 		end
 
 		class UrlEncoded < Base
-
 			def build_stream(params)
 				@stream = StringIO.new(params.map do |k,v| 
 					"#{escape(k)}=#{escape(v)}"
 				end.join("&"))
 				@stream.seek(0)
 			end
-			
+
 			def headers
-				super.merge({'Content-Type' => 'application/x-www-form-urlencoded'})
+				super.merge({ 'Content-Type' => 'application/x-www-form-urlencoded' })
 			end
-			
 		end
 
 		class Multipart < Base
-			
 			EOL = "\r\n"
-			
+
 			def build_stream(params)
 				b = "--#{boundary}"
-				
+
 				@stream = Tempfile.new("RESTClient.Stream.#{rand(1000)}")
 				@stream.write(b + EOL)
 				params.each do |k,v|
@@ -89,7 +81,7 @@ module RestClient
 				@stream.write('--')
 				@stream.seek(0)
 			end
-			
+
 			def create_regular_field(s, k, v)
 				s.write("Content-Disposition: multipart/form-data; name=\"#{k}\"")
 				s.write(EOL)
@@ -109,7 +101,7 @@ module RestClient
 					v.close
 				end
 			end
-			
+
 			def mime_for(path)
 				ext = File.extname(path)[1..-1]
 				MIME_TYPES[ext] || 'text/plain'
@@ -118,79 +110,74 @@ module RestClient
 			def boundary
 				@boundary ||= rand(1_000_000).to_s
 			end
-			
+
 			def headers
 				super.merge({'Content-Type' => %Q{multipart/form-data; boundary="#{boundary}"}})
 			end
-			
+
 			def close
 				@stream.close
 			end
-
 		end
 
 		# :stopdoc:
-    # From WEBrick.
-    MIME_TYPES = {
-      "ai"    => "application/postscript",
-      "asc"   => "text/plain",
-      "avi"   => "video/x-msvideo",
-      "bin"   => "application/octet-stream",
-      "bmp"   => "image/bmp",
-      "class" => "application/octet-stream",
-      "cer"   => "application/pkix-cert",
-      "crl"   => "application/pkix-crl",
-      "crt"   => "application/x-x509-ca-cert",
-     #"crl"   => "application/x-pkcs7-crl",
-      "css"   => "text/css",
-      "dms"   => "application/octet-stream",
-      "doc"   => "application/msword",
-      "dvi"   => "application/x-dvi",
-      "eps"   => "application/postscript",
-      "etx"   => "text/x-setext",
-      "exe"   => "application/octet-stream",
-      "gif"   => "image/gif",
-      "gz"   => "application/x-gzip",
-      "htm"   => "text/html",
-      "html"  => "text/html",
-      "jpe"   => "image/jpeg",
-      "jpeg"  => "image/jpeg",
-      "jpg"   => "image/jpeg",
-      "js"    => "text/javascript",
-      "lha"   => "application/octet-stream",
-      "lzh"   => "application/octet-stream",
-      "mov"   => "video/quicktime",
-      "mpe"   => "video/mpeg",
-      "mpeg"  => "video/mpeg",
-      "mpg"   => "video/mpeg",
-      "pbm"   => "image/x-portable-bitmap",
-      "pdf"   => "application/pdf",
-      "pgm"   => "image/x-portable-graymap",
-      "png"   => "image/png",
-      "pnm"   => "image/x-portable-anymap",
-      "ppm"   => "image/x-portable-pixmap",
-      "ppt"   => "application/vnd.ms-powerpoint",
-      "ps"    => "application/postscript",
-      "qt"    => "video/quicktime",
-      "ras"   => "image/x-cmu-raster",
-      "rb"    => "text/plain",
-      "rd"    => "text/plain",
-      "rtf"   => "application/rtf",
-      "sgm"   => "text/sgml",
-      "sgml"  => "text/sgml",
-      "tif"   => "image/tiff",
-      "tiff"  => "image/tiff",
-      "txt"   => "text/plain",
-      "xbm"   => "image/x-xbitmap",
-      "xls"   => "application/vnd.ms-excel",
-      "xml"   => "text/xml",
-      "xpm"   => "image/x-xpixmap",
-      "xwd"   => "image/x-xwindowdump",
-      "zip"   => "application/zip",
-    }
-    # :startdoc:
-		
-
+		# From WEBrick.
+		MIME_TYPES = {
+			"ai"    => "application/postscript",
+			"asc"   => "text/plain",
+			"avi"   => "video/x-msvideo",
+			"bin"   => "application/octet-stream",
+			"bmp"   => "image/bmp",
+			"class" => "application/octet-stream",
+			"cer"   => "application/pkix-cert",
+			"crl"   => "application/pkix-crl",
+			"crt"   => "application/x-x509-ca-cert",
+			"css"   => "text/css",
+			"dms"   => "application/octet-stream",
+			"doc"   => "application/msword",
+			"dvi"   => "application/x-dvi",
+			"eps"   => "application/postscript",
+			"etx"   => "text/x-setext",
+			"exe"   => "application/octet-stream",
+			"gif"   => "image/gif",
+			"gz"   => "application/x-gzip",
+			"htm"   => "text/html",
+			"html"  => "text/html",
+			"jpe"   => "image/jpeg",
+			"jpeg"  => "image/jpeg",
+			"jpg"   => "image/jpeg",
+			"js"    => "text/javascript",
+			"lha"   => "application/octet-stream",
+			"lzh"   => "application/octet-stream",
+			"mov"   => "video/quicktime",
+			"mpe"   => "video/mpeg",
+			"mpeg"  => "video/mpeg",
+			"mpg"   => "video/mpeg",
+			"pbm"   => "image/x-portable-bitmap",
+			"pdf"   => "application/pdf",
+			"pgm"   => "image/x-portable-graymap",
+			"png"   => "image/png",
+			"pnm"   => "image/x-portable-anymap",
+			"ppm"   => "image/x-portable-pixmap",
+			"ppt"   => "application/vnd.ms-powerpoint",
+			"ps"    => "application/postscript",
+			"qt"    => "video/quicktime",
+			"ras"   => "image/x-cmu-raster",
+			"rb"    => "text/plain",
+			"rd"    => "text/plain",
+			"rtf"   => "application/rtf",
+			"sgm"   => "text/sgml",
+			"sgml"  => "text/sgml",
+			"tif"   => "image/tiff",
+			"tiff"  => "image/tiff",
+			"txt"   => "text/plain",
+			"xbm"   => "image/x-xbitmap",
+			"xls"   => "application/vnd.ms-excel",
+			"xml"   => "text/xml",
+			"xpm"   => "image/x-xpixmap",
+			"xwd"   => "image/x-xwindowdump",
+			"zip"   => "application/zip",
+		}
+		# :startdoc:
 	end
-
 end
