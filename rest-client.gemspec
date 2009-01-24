be753027929b85568e90d04a2b22ba105ea67b19
Adam Wiggins <adam@heroku.com>
2009-01-24 14:55:18 -0800

    organize specs to match lib

--- rest-client.gemspec
+++ rest-client.gemspec
@@ -10,8 +10,10 @@ Gem::Specification.new do |s|
     s.has_rdoc = true
     s.platform = Gem::Platform::RUBY
     s.files = %w(Rakefile README.rdoc rest-client.gemspec
-                 lib/request_errors.rb lib/resource.rb lib/rest_client.rb 
-                 spec/base.rb spec/request_errors_spec.rb spec/resource_spec.rb spec/rest_client_spec.rb
+                 lib/restclient/request.rb lib/restclient/response.rb
+                 lib/restclient/exceptions.rb lib/restclient/resource.rb
+                 spec/base.rb spec/request_spec.rb spec/response_spec.rb
+                 spec/exceptions_spec.rb spec/resource_spec.rb spec/restclient_spec.rb
                  bin/restclient)
     s.executables = ['restclient']
     s.require_path = "lib"
---
+++ spec/response_spec.rb
@@ -0,0 +1,36 @@
+require File.dirname(__FILE__) + '/base'
+
+describe RestClient::Response do
+	before do
+		@net_http_res = mock('net http response')
+		@response = RestClient::Response.new('abc', @net_http_res)
+	end
+
+	it "behaves like string" do
+		@response.should == 'abc'
+	end
+
+	it "fetches the numeric response code" do
+		@net_http_res.should_receive(:code).and_return('200')
+		@response.code.should == 200
+	end
+
+	it "beautifies the headers by turning the keys to symbols" do
+		h = RestClient::Response.beautify_headers('content-type' => [ 'x' ])
+		h.keys.first.should == :content_type
+	end
+
+	it "beautifies the headers by turning the values to strings instead of one-element arrays" do
+		h = RestClient::Response.beautify_headers('x' => [ 'text/html' ] )
+		h.values.first.should == 'text/html'
+	end
+
+	it "fetches the headers" do
+		@net_http_res.should_receive(:to_hash).and_return('content-type' => [ 'text/html' ])
+		@response.headers.should == { :content_type => 'text/html' }
+	end
+
+	it "can access the net http result directly" do
+		@response.net_http_res.should == @net_http_res
+	end
+end
