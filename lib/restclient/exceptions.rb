db3d8adad87e229b0490292ceef88d20d1c922cb
Julien Kirch <code@archiloque.net>
2010-01-20 18:38:20 +0100

    better formatting

--- lib/restclient/exceptions.rb
+++ lib/restclient/exceptions.rb
@@ -29,14 +29,31 @@ module RestClient
     EXCEPTIONS_MAP = {}
   end
 
-  {304 => 'Not Modified', 305 => 'Use Proxy',
-   400 => 'Bad Request', 401 => 'Unauthorized', 403 => 'Forbidden', 404 => 'Resource Not Found',
-   405 => 'Method Not Allowed', 406 => 'Not Acceptable', 407 => 'Proxy Authentication Required', 408 => 'Request Timeout',
-   409 => 'Conflict', 410 => 'Gone', 411 => 'Length Required', 412 => 'Precondition Failed',
-   413 => 'Request Entity Too Large', 414 => 'Request-URI Too Long', 415 => 'Unsupported Media Type', 416 => 'Requested Range Not Satisfiable',
+  {304 => 'Not Modified',
+   305 => 'Use Proxy',
+   400 => 'Bad Request',
+   401 => 'Unauthorized',
+   403 => 'Forbidden',
+   404 => 'Resource Not Found',
+   405 => 'Method Not Allowed',
+   406 => 'Not Acceptable',
+   407 => 'Proxy Authentication Required',
+   408 => 'Request Timeout',
+   409 => 'Conflict',
+   410 => 'Gone',
+   411 => 'Length Required',
+   412 => 'Precondition Failed',
+   413 => 'Request Entity Too Large',
+   414 => 'Request-URI Too Long',
+   415 => 'Unsupported Media Type',
+   416 => 'Requested Range Not Satisfiable',
    417 => 'Expectation Failed',
-   500 => 'Internal Server Error', 501 => 'Not Implemented', 502 => 'Bad Gateway', 503 => 'Service Unavailable',
-   504 => 'Gateway Timeout', 505 => 'HTTP Version Not Supported'}.each_pair do |code, message|
+   500 => 'Internal Server Error',
+   501 => 'Not Implemented',
+   502 => 'Bad Gateway',
+   503 => 'Service Unavailable',
+   504 => 'Gateway Timeout',
+   505 => 'HTTP Version Not Supported'}.each_pair do |code, message|
     klass = Class.new(Exception) do
       send :define_method, :message, Proc.new{message}
     end
