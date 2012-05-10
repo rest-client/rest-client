b8e3a4efd0dae5e5b7757351630b8f0208e0f366
Michael Schuerig <michael@schuerig.de>
2012-05-10 21:56:53 +0200

    Set the encoding of the response to the one given in charset header.

--- lib/restclient/response.rb
+++ lib/restclient/response.rb
@@ -14,6 +14,11 @@ module RestClient
 
     def Response.create body, net_http_res, args
       result = body || ''
+      if result.respond_to?(:force_encoding)
+        if (charset = net_http_res.type_params['charset'])
+          result.force_encoding(charset)
+        end
+      end
       result.extend Response
       result.net_http_res = net_http_res
       result.args = args
