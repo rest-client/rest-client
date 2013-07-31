30cfdd5e72f064bb90c918cffc195dd04bdad516
Larry Gilbert <larry@L2G.to>
2013-07-31 09:58:25 -0700

    Changed use of spec/base.rb into spec/spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,10 @@ def is_ruby_19?
   RUBY_VERSION > '1.9'
 end
 
-require 'rubygems'
-
 begin
   require "ruby-debug"
 rescue LoadError
   # NOP, ignore
 end
 
-require File.dirname(__FILE__) + '/../lib/restclient'
+require 'restclient'
