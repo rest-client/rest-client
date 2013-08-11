c6a791f7524795f856d2d98334b50389b5ebf74b
Jon Rowe <hello@jonrowe.co.uk>
2013-08-11 21:16:02 +0100

    convert == to eq to avoid warnings, (all were done for concistency)
    (cherry picked from commit 0b6e74593afcc71b307b999b658157257d52549a)
    Signed-off-by: Larry Gilbert <larry@l2g.to>
    
    Conflicts:
    	spec/request_spec.rb

--- spec/abstract_response_spec.rb
+++ spec/abstract_response_spec.rb
@@ -22,7 +22,7 @@ describe RestClient::AbstractResponse do
 
   it "fetches the numeric response code" do
     @net_http_res.should_receive(:code).and_return('200')
-    @response.code.should == 200
+    @response.code.should eq 200
   end
 
   it "has a nice description" do
@@ -33,36 +33,36 @@ describe RestClient::AbstractResponse do
 
   it "beautifies the headers by turning the keys to symbols" do
     h = RestClient::AbstractResponse.beautify_headers('content-type' => [ 'x' ])
-    h.keys.first.should == :content_type
+    h.keys.first.should eq :content_type
   end
 
   it "beautifies the headers by turning the values to strings instead of one-element arrays" do
     h = RestClient::AbstractResponse.beautify_headers('x' => [ 'text/html' ] )
-    h.values.first.should == 'text/html'
+    h.values.first.should eq 'text/html'
   end
 
   it "fetches the headers" do
     @net_http_res.should_receive(:to_hash).and_return('content-type' => [ 'text/html' ])
-    @response.headers.should == { :content_type => 'text/html' }
+    @response.headers.should eq({ :content_type => 'text/html' })
   end
 
   it "extracts cookies from response headers" do
     @net_http_res.should_receive(:to_hash).and_return('set-cookie' => ['session_id=1; path=/'])
-    @response.cookies.should == { 'session_id' => '1' }
+    @response.cookies.should eq({ 'session_id' => '1' })
   end
 
   it "extract strange cookies" do
     @net_http_res.should_receive(:to_hash).and_return('set-cookie' => ['session_id=ZJ/HQVH6YE+rVkTpn0zvTQ==; path=/'])
-    @response.cookies.should == { 'session_id' => 'ZJ%2FHQVH6YE+rVkTpn0zvTQ%3D%3D' }
+    @response.cookies.should eq({ 'session_id' => 'ZJ%2FHQVH6YE+rVkTpn0zvTQ%3D%3D' })
   end
 
   it "doesn't escape cookies" do
     @net_http_res.should_receive(:to_hash).and_return('set-cookie' => ['session_id=BAh7BzoNYXBwX25hbWUiEGFwcGxpY2F0aW9uOgpsb2dpbiIKYWRtaW4%3D%0A--08114ba654f17c04d20dcc5228ec672508f738ca; path=/'])
-    @response.cookies.should == { 'session_id' => 'BAh7BzoNYXBwX25hbWUiEGFwcGxpY2F0aW9uOgpsb2dpbiIKYWRtaW4%3D%0A--08114ba654f17c04d20dcc5228ec672508f738ca' }
+    @response.cookies.should eq({ 'session_id' => 'BAh7BzoNYXBwX25hbWUiEGFwcGxpY2F0aW9uOgpsb2dpbiIKYWRtaW4%3D%0A--08114ba654f17c04d20dcc5228ec672508f738ca' })
   end
 
   it "can access the net http result directly" do
-    @response.net_http_res.should == @net_http_res
+    @response.net_http_res.should eq @net_http_res
   end
   
   describe "#return!" do
