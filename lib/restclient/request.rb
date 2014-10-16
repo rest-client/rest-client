3ab5a0cc109cb0d3862842f66c67c743b7ebde76
Rafael Santos <formigarafa@gmail.com>
2014-10-16 18:24:59 +1300

    updated protocol version to tls
    this will solve the issues with server not responding to sslv3 due to bug found in the protocol (POODLE)

--- lib/restclient/request.rb
+++ lib/restclient/request.rb
@@ -56,7 +56,7 @@ module RestClient
       @ssl_client_key = args[:ssl_client_key] || nil
       @ssl_ca_file = args[:ssl_ca_file] || nil
       @ssl_ca_path = args[:ssl_ca_path] || nil
-      @ssl_version = args[:ssl_version] || 'SSLv3'
+      @ssl_version = args[:ssl_version] || 'TLSv1_2'
       @tf = nil # If you are a raw request, this is your tempfile
       @max_redirects = args[:max_redirects] || 10
       @processed_headers = make_headers headers
