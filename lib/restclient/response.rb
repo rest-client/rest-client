0a9e09666987bbe4601cbf3621b3de047a94b3ba
Andy Brody <git@abrody.com>
2015-05-27 18:37:20 -0700

    Make the Response actually a class not a module.
    Making Response a module was always super questionable. Removing the use
    of extend should improve performance and clarity.
    
    Fixes: #225

--- lib/restclient/response.rb
+++ lib/restclient/response.rb
@@ -2,7 +2,7 @@ module RestClient
 
   # A Response from RestClient, you can access the response body, the code or the headers.
   #
-  module Response
+  class Response < String
 
     include AbstractResponse
 
@@ -38,9 +38,8 @@ module RestClient
       "<RestClient::Response #{code.inspect} #{body_truncated(10).inspect}>"
     end
 
-    def self.create body, net_http_res, args, request
-      result = body || ''
-      result.extend Response
+    def self.create(body, net_http_res, args, request)
+      result = self.new(body || '')
 
       result.response_set_vars(net_http_res, args, request)
       fix_encoding(result)
