43e61320420c77d2b9bc93e95ecee884e972286f
Andy Brody <git@abrody.com>
2015-11-08 04:04:23 -0800

    Add new featureful implementation of HTTP params.

--- lib/restclient/utils.rb
+++ lib/restclient/utils.rb
@@ -89,5 +89,91 @@ module RestClient
 
       [key, pdict]
     end
+
+    # Serialize a ruby object into HTTP query string parameters.
+    #
+    # There is no standard for doing this, so we choose our own slightly
+    # idiosyncratic format. The output closely matches the format understood by
+    # Rails, Rack, and PHP.
+    #
+    # If you don't want handling of complex objects and only want to handle
+    # simple flat hashes, you may want to use `URI.encode_www_form` instead,
+    # which implements HTML5-compliant URL encoded form data.
+    #
+    # @param [Object] object The object to serialize
+    # @param [String] parent_key The parent hash key of this object
+    #
+    # @return [String] A string appropriate for use as an HTTP query string
+    #
+    # @see URI.encode_www_form
+    #
+    # @see See also Object#to_query in ActiveSupport
+    # @see http://php.net/manual/en/function.http-build-query.php
+    #   http_build_query in PHP
+    #
+    # Notable differences from the ActiveSupport implementation:
+    #
+    # - Empty hash and empty array are treated the same as nil instead of being
+    #   omitted entirely from the output. Rather than disappearing, they will
+    #   appear to be nil instead.
+    #
+    # @since 2.0.0
+    #
+    # @example Simple hashes
+    #   >> encode_query_string({foo: 123, bar: 456})
+    #   => 'foo=123&bar=456'
+    #
+    # @example Simple arrays
+    #   >> encode_query_string({foo: [1,2,3]})
+    #   => 'foo[]=1&foo[]=2&foo[]=3'
+    #
+    # @example Nested hashes
+    #   >> encode_query_string({outer: {foo: 123, bar: 456}})
+    #   => 'outer[foo]=123&outer[bar]=456'
+    #
+    # @example Null and empty values
+    #   >> encode_query-string({string: '', empty: nil, list: [], hash: {}})
+    #   => 'string=&empty&list&hash'
+    #
+    # @example Nested nulls
+    #   >> encode_query-string({foo: {string: '', empty: nil}})
+    #   => 'foo[string]=&foo[empty]'
+    #
+    def self.encode_query_string(object, parent_key=nil)
+      if !parent_key && !object.is_a?(Hash)
+        raise ArgumentError.new('top level query param must be a Hash, got: ' +
+                                object.inspect)
+      end
+
+      case object
+      when Hash
+        if object.empty?
+          if parent_key
+            return encode_query_string(nil, parent_key)
+          else
+            return ''
+          end
+        end
+
+        return object.map { |k, v|
+          k = CGI.escape(k.to_s)
+          encode_query_string(v, parent_key ? "#{parent_key}[#{k}]" : k)
+        }.join('&')
+
+      when Array
+        if object.empty?
+          return encode_query_string(nil, parent_key)
+        end
+
+        prefix = "#{parent_key}[]"
+        return object.map {|v| encode_query_string(v, prefix) }.join('&')
+
+      when nil
+        parent_key.to_s
+
+      else
+        "#{parent_key}=#{CGI.escape(object.to_s)}"
+      end
+    end
   end
 end
