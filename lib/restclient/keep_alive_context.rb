67d249ecf9e0d2078acfcc9d6c5b2246d5cf48f5
AlunYou <xianlun.you@autodesk.com>
2016-03-21 16:59:46 +0800

    support keep-alive by create a context object which wraps the requests

---
+++ lib/restclient/keep_alive_context.rb
@@ -0,0 +1,44 @@
+module RestClient
+   class KeepAliveContext
+     attr_reader :host_hash
+
+     def initialize
+       @host_hash = {}
+     end
+
+     def execute(args, & block)
+       unless args[:url]
+         raise ArgumentError, "must pass :url"
+       end
+       uri = Utils.parse_url(args[:url])
+
+       identifier = "#{uri.scheme}://#{uri.hostname}:#{uri.port}"
+       http_object = @host_hash[identifier]
+
+       request = Request.new(args.merge(http_object: http_object, keep_alive: true))
+       response = request.execute(& block)
+       @host_hash[identifier] = request.http_object
+       response
+     end
+
+     def finish
+       @host_hash.each do |key, http_object|
+         http_object.finish if http_object
+         puts key
+       end
+     end
+
+     def self.start
+       KeepAliveContext context = KeepAliveContext.new
+       if block_given?
+         begin
+           return yield(context)
+         ensure
+           context.finish
+         end
+       else
+         raise ArgumentError, "must pass block"
+       end
+     end
+   end
+end
