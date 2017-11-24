108a9c56add3f0c69949f65c472ce2c2774a7cf9
Magne Matre Gåsland <magnemg@gmail.com>
2017-11-24 13:07:27 +0100

    Fix logging output, so that it logs the timestamp on a separate line. To preserve the ability to copy/paste log statements into the rails console for manual execution.

--- lib/restclient/request.rb
+++ lib/restclient/request.rb
@@ -535,10 +535,10 @@ module RestClient
     def log_request
       return unless log
 
-      out = []
-
       time = Time.zone.now
-      out << "# " << time.strftime("%Y-%m-%dT%H:%M:%S.") << "%06d" % time.usec
+      log << "# " + time.strftime("%Y-%m-%dT%H:%M:%S.") + "%06d" % time.usec
+
+      out = []
       out << "RestClient.#{method} #{redacted_url.inspect}"
       out << payload.short_inspect if payload
       out << processed_headers.to_a.sort.map { |(k, v)| [k.inspect, v.inspect].join("=>") }.join(", ")
